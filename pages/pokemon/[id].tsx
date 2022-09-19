35d860a24e65bf5bbafc42ebfb9e5358dcbbb15c
ary_barros <ary_barros@external.mckinsey.com>
2022-09-19 11:07:36 -0300

    🎨 Implementing sprite query

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,49 @@
+import { gql } from "@apollo/client";
+import Image from "next/image";
+import client from "../../apollo-client";
+import { PokemonRequest } from "../../types/PokemonRequest";
+
+export default function Pokemon({ pokemon, sprite }: PokemonRequest) {
+  console.log(pokemon, sprite);
+  return (
+    <>
+      <section className="cl-black">
+        {pokemon.name}
+      </section>
+      <Image src={sprite} alt={pokemon.name} width={200} height={200} />
+    </>
+  );
+}
+
+export async function getServerSideProps({ params }: { params: { id: string } }) {
+  const sprite = `https://raw.githubusercontent.com/geekygreek7/animated-pokemon-gifs/master/${params.id}.gif`
+
+  const { data } = await client.query({
+    query: gql`
+                query GetPokemon {
+                  pokemon_v2_pokemon(where: {id: {_eq: ${params.id}}}) {
+                  id
+          name
+                pokemon_v2_pokemonstats {
+                  base_stat
+            pokemon_v2_stat {
+                  name
+                }
+          }
+                pokemon_v2_pokemontypes {
+                  pokemon_v2_type {
+                  name
+                }
+          }
+        }
+      }
+                `
+  });
+
+  return {
+    props: {
+      pokemon: data.pokemon_v2_pokemon[0],
+      sprite
+    }
+  }
+}
---
+++ types/PokemonRequest.ts
@@ -0,0 +1,27 @@
+export type PokemonRequest = {
+  pokemon: {
+    __typename: string;
+    id: number;
+    name: string;
+    "pokemon_v2_pokemonstats": PokemonStats[];
+    "pokemon_v2_pokemontypes": PokemonTypes[];
+  },
+  sprite: string;
+}
+
+export type PokemonStats = {
+  __typename: string;
+  base_stat: number;
+  pokemon_v2_stat: {
+    __typename: string;
+    name: string;
+  }
+}
+
+export type PokemonTypes = {
+  __typename: string;
+  pokemon_v2_type: {
+    __typename: string;
+    name: string;
+  }
+}
